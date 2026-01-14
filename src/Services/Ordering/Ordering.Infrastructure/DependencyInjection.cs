47c4512fb67c82b487ba0fe000e0861f0014d055
Heitor Giacomini <heitorgiacomini@gmail.com>
2026-01-14 07:27:10 -0300

    feat: Implement Ordering System with EF Core and CQRS
    - Added GetOrdersByNameQuery and GetOrdersByNameResult for querying orders by name.
    - Created ApplicationDbContext for managing database interactions.
    - Configured entity mappings for Customer, Order, OrderItem, and Product using Fluent API.
    - Implemented database initialization and seeding logic in DatabaseExtensions.
    - Added initial data for Customers, Products, and Orders with items.
    - Introduced AuditableEntityInterceptor for automatic tracking of entity changes.
    - Implemented DispatchDomainEventsInterceptor for handling domain events during save operations.
    - Created initial migration for setting up the database schema.
    - Added global using directives for cleaner code organization.

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure;
 public static class DependencyInjection
@@ -9,11 +11,17 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
-        //// Add services to the container.
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer(connectionString));
+        // Add services to the container.
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
 
-        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services;
     }
